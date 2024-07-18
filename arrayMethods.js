12a5908c5f94208f95d0bd47f94d2c321ca11973
Adrián Maza <adri_mv1@hotmail.com>
2024-07-18 17:54:53 +0200

    array methods

--- arrayMethods.js
+++ arrayMethods.js
@@ -55,3 +55,76 @@ console.log(countryLength);
 
 const productNameArray = products.map((product) => product.name);
 console.log(productNameArray);
+
+
+// filter
+
+const filteredNums = numArray.filter((number) => number > 15);
+console.log(filteredNums);
+
+const filteredCountries = countries.filter((country) => country.length >= 4);
+console.log(filteredCountries);
+
+const forSaleProducts = products.filter((product) => product.isForSale);
+console.log(forSaleProducts);
+
+
+// reduce
+
+console.log(numArray);
+
+const sumResult = numArray.reduce((accumulator, currentValue) => {
+  return currentValue + accumulator;
+}, 0)
+console.log(sumResult);
+
+const multiplyResult = numArray.reduce((acc, cv) => {
+  return cv * acc;
+}, 1)
+console.log(multiplyResult);
+
+const concatCountries = countries.reduce((acc, cv) => {
+  return cv + acc;
+}, "")
+console.log(concatCountries);
+
+
+console.log(products);
+const totalPrice = products.reduce((acc, cv) => {
+  return cv.price + acc;
+}, 0)
+console.log(totalPrice);
+
+
+// sort
+
+const numbers = [1, 100000, 25, 3000, 27, 12];
+numbers.sort((a, b) => {
+  return b - a;
+});
+console.log(numbers);
+
+countries.sort((a, b) => {
+  return b.length - a.length;
+})
+console.log(countries);
+
+products.sort((a, b) => {
+  return b.price - a.price;
+})
+console.log(products);
+
+
+// create a shallow copy of array
+
+const productsCopy = products.map((product) => {
+  return {...product};
+});
+console.log(productsCopy);
+
+
+// reverse
+
+console.log(countries);
+countries.reverse();
+console.log(countries);
